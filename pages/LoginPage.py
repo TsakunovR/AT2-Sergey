8d376f04b27535ee52c0ea8669f21a50c8884d03
Sergey-Konev-D1 <skon88@mail.ru>
2023-11-12 22:22:17 +0300

    Добавлены локаторы + новый класс LoginPageLocators

---
+++ pages/LoginPage.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+class LoginPageLocators:
+    LOGIN_TAB = (By.XPATH, '//a[@data-l="t,login_tab"]')
+    FIELD_LOGIN = (By.XPATH, '//a[@data-l="t,login_tab"]')
+    FIELD_PASSWORD = (By.XPATH, '//input[@data-l="t,password"]')
+    SIGN_IN_BUTTON = (By.XPATH, '//input[@data-l="t,sign_in"]')
+    SIGN_IN_BUTTON_QR = (By.XPATH, '//a[@data-l="t,get_qr"]')
+    CANT_SIGN_IN = (By.XPATH, '//a[@data-l="t,restore"]')
+    REGISTRATION = (By.XPATH, '//a[@data-l="t,register"]')
+    VK_BUTTON = (By.XPATH, '//a[@data-l="t,vkc"]')
+    MAIL_BUTTON = (By.XPATH, '//a[@data-l="t,mailru"]')
+    GOOGLE_BUTTON = (By.XPATH, '//a[@data-l="t,google"]')
+    YANDEX_BUTTON = (By.XPATH, '//a[@data-l="t,yandex"]')
+    APPLE_BUTTON = (By.XPATH, '//a[@data-l="t,apple"]')
+    QR_TAB = (By.XPATH, '//a[@data-l="t,qr_tab"]')
+
+
+
+
+
